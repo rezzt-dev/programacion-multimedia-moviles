68ec59aeeddd94f031440f97a9d3c7b601d8de70
juangc.zip <jgarciac111@educastillalamancha.es>
2024-11-04 12:51:05 +0100

    a

---
+++ unidad-2/ejercicios-toolbar/ejercicio-1/app/src/main/java/com/jgc/ejerciciotoolbar/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.jgc.ejerciciotoolbar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.jgc.ejerciciotoolbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val toolbar: MaterialToolbar = binding.materialToolbar
+    setSupportActionBar(toolbar)
+  }
+}
